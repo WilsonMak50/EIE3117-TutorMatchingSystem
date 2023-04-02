2f6c985c0550bfd35f8ecef789ae0746949f8180
WilsonMak50 <118324692+WilsonMak50@users.noreply.github.com>
2023-04-02 20:24:46 +0800

    XSS and SQL injection Prevention in std and tut php

--- tut.php
+++ tut.php
@@ -35,24 +35,34 @@
     
     // If upload button is clicked ...
     if (isset($_POST['upload'])) {
-    
-        $filename = $_FILES["uploadfile"]["name"];
-        $tempname = $_FILES["uploadfile"]["tmp_name"];
-        $folder = "./upimage/" . $filename;
+        // Get the filename and temporary name of the uploaded file
+        $filename = mysqli_real_escape_string($connect, $_FILES["uploadfile"]["name"]);
+        $tempname = mysqli_real_escape_string($connect,$_FILES["uploadfile"]["tmp_name"]);
 
-        $sql="UPDATE tutor SET img=? WHERE uid=? ; ";
-        $stmt=mysqli_stmt_init($connect);
-        if(!mysqli_stmt_prepare($stmt,$sql)){          
-            echo'error cannot prepare the sql stmt';
-            exit();
-        }else{
-
-            echo $filename;
-            mysqli_stmt_bind_param($stmt,"ss",$filename,$myUid);
-            mysqli_stmt_execute($stmt);
-
+        // Validate the file type to ensure only image files are uploaded
+        $fileType = strtolower(pathinfo($filename,PATHINFO_EXTENSION));
+        if($fileType != "jpg" && $fileType != "png" && $fileType != "jpeg" && $fileType != "gif" ) {
+            echo "<h3> Only JPG, JPEG, PNG & GIF files are allowed!</h3>";
+            return;
         }
 
+        // Generate a unique filename to prevent overwriting existing files
+        $newFilename = uniqid('', true) . '.' . $fileType;
+        $folder = "./upimage/" . $newFilename;
+
+        // Get the file size to ensure it's not too large
+        $fileSize = $_FILES["uploadfile"]["size"];
+        if($fileSize > 500000) {
+            echo "<h3> Your file is too large!</h3>";
+            return;
+        }
+
+        // Insert the filename into the database
+        $stmt = $connect->prepare("INSERT INTO image (filename) VALUES (?)");
+        $stmt->bind_param("s", $newFilename);
+        $stmt->execute();
+
+        // Now let's move the uploaded image into the folder: upimage
         if (move_uploaded_file($tempname, $folder)) {
             echo "<h3> Image uploaded successfully!</h3>";
         } else {
@@ -61,7 +71,7 @@
     }
 
     if(isset($_POST['up-profile'])){
-        $myIntro=$_POST['des'];
+        $myIntro=urlencode($_POST['des']);
         $checkbox1=$_POST['sub'];  
         $chk="";  
 
